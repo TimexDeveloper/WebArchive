238075240c5fe5b1ab3e23d1ec0532e8c6ce8db9
Timex <timexdev123@gmail.com>
2026-01-08 18:00:21 +0700

    Bug Fix

--- script/loader.lua
+++ script/loader.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 2413927524 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TimexDeveloper/WebArchive/refs/heads/main/script/TheRake.lua"))()
 elseif game.PlaceId == 1962086868 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TimexDeveloper/WebArchive/refs/heads/main/script/TowerOfHell.lua.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TimexDeveloper/WebArchive/refs/heads/main/script/TowerOfHell.lua"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TimexDeveloper/WebArchive/refs/heads/main/script/universal.lua"))()
 end
