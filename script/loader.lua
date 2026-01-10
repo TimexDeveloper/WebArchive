b788118235c9e14628d95eb02b0a64eb2598b6d3
Timex <timexdev123@gmail.com>
2026-01-10 13:51:10 +0300

    - Removed Universal - Added Grace support

--- script/loader.lua
+++ script/loader.lua
@@ -1,7 +1,10 @@
-if game.PlaceId == 2413927524 then
+if game.PlaceId == 2413927524 then --The Rake
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TimexDeveloper/WebArchive/refs/heads/main/script/TheRake.lua"))()
-elseif game.PlaceId == 1962086868 then
+elseif game.PlaceId == 1962086868 then --TOH
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TimexDeveloper/WebArchive/refs/heads/main/script/TowerOfHell.lua"))()
+elseif game.PlaceId == 110333320616502 then --Grace
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TimexDeveloper/WebArchive/refs/heads/main/script/Grace.lua"))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TimexDeveloper/WebArchive/refs/heads/main/script/universal.lua"))()
+    --loadstring(game:HttpGet("https://raw.githubusercontent.com/TimexDeveloper/WebArchive/refs/heads/main/script/universal.lua"))()
+    print("Game unsupported")
 end
